842a9028155149853617f6f8166e56e6edeb5ca0
an <4361526@naver.com>
2026-01-12 21:12:27 +0900

    v1

--- prisma.config.ts
+++ prisma.config.ts
@@ -8,7 +8,6 @@ export default defineConfig({
     path: "prisma/migrations",
   },
   datasource: {
-    url: process.env["DATABASE_URL"],
-    directUrl: process.env["DIRECT_URL"],
+    url: process.env["DIRECT_URL"] ?? process.env["DATABASE_URL"],
   },
 });
