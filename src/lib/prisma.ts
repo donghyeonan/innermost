4ea110059034af01bcf1f0cc1153b6e60a31386e
an <4361526@naver.com>
2026-01-12 21:27:03 +0900

    v2

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -5,7 +5,13 @@ const globalForPrisma = globalThis as typeof globalThis & {
     prisma?: PrismaClient
 }
 
-export const prisma: PrismaClient = globalForPrisma.prisma ?? new PrismaClient()
+export const prisma: PrismaClient = globalForPrisma.prisma ?? new PrismaClient({
+    datasources: {
+        db: {
+            url: process.env.DATABASE_URL,
+        },
+    },
+})
 
 if (process.env.NODE_ENV !== 'production') {
     globalForPrisma.prisma = prisma
