d751116b9155866ad0e072dca4a2abfd4421085f
an <4361526@naver.com>
2026-01-12 21:23:44 +0900

    fix

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,17 +1,11 @@
 import { PrismaClient } from '@prisma/client'
-import { PrismaPg } from '@prisma/adapter-pg'
+
 
 const globalForPrisma = globalThis as typeof globalThis & {
     prisma?: PrismaClient
 }
 
-const adapter = new PrismaPg({
-    connectionString: process.env.DATABASE_URL,
-})
-
-export const prisma: PrismaClient = globalForPrisma.prisma ?? new PrismaClient({
-    adapter,
-})
+export const prisma: PrismaClient = globalForPrisma.prisma ?? new PrismaClient()
 
 if (process.env.NODE_ENV !== 'production') {
     globalForPrisma.prisma = prisma
